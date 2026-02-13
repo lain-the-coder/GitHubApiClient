02a5587fb7ca33ad1ade878e208ed80e025f5b1a
Ehab Ahmed <ahmed.ehabj@gmail.com>
2026-02-13 14:53:02 +0400

    Created service file shell and registered service for DI along with ihttpclient factory

---
+++ DTOs/GitHubUserDTO.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace GitHubApiClient.DTOs
+{
+    public class GitHubUserDTO
+    {
+        [JsonPropertyName("login")]
+        public required string Login { get; set; }
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+        [JsonPropertyName("bio")]
+        public string? Bio { get; set; }
+        [JsonPropertyName("public_repos")]
+        public int PublicRepos { get; set; }
+        [JsonPropertyName("followers")]
+        public int Followers { get; set; }
+        [JsonPropertyName("following")]
+        public int Following { get; set; }
+    }
+}
---
+++ Services/GitHubService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using GitHubApiClient.DTOs;
+
+
+namespace GitHubApiClient.Services
+{
+    public class GitHubService
+    {
+        private readonly HttpClient _httpClient;
+        public GitHubService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://api.github.com/");
+        }
+        public async Task<GitHubUserDTO?> GetAuthenticatedUserAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
